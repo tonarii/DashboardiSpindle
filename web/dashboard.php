f232a40a19ce68d19fda2047605a7577183b69e4
tonarii <mydogsk8@hotmail.com>
2018-11-02 10:00:59 +0100

    Update dashboard.php

--- web/dashboard.php
+++ web/dashboard.php
@@ -144,6 +144,7 @@ $imghops = '<div class="hoppng" id="hoppng"><img src="../assets/img/icons-hops-b
   <script src="../assets/js/fusioncharts.theme.fusion.js"></script>
   <script src="../assets/js/fusioncharts.powercharts.js"></script>
   <script src="../assets/js/fusioncharts.charts.js"></script>
+  <script src="../assets/js/fusioncharts.widgets.js"></script>
   <script src="../assets/js/moment.min.js"></script>
   <script src="../assets/js/moment-timezone-with-data.js"></script>
   <script src="../assets/js/highcharts.js"></script>
