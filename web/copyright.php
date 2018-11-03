1e20742e99fe095bab5a36588a2da505735607ad
tonarii <mydogsk8@hotmail.com>
2018-11-03 11:30:10 +0100

    Update copyright.php

--- web/copyright.php
+++ web/copyright.php
@@ -266,13 +266,7 @@
       });
     });
   </script>
-  <script>
-    $(document).ready(function() {
-      // Javascript method's body can be found in assets/js/demos.js
-      demo.initDashboardPageCharts();
 
-    });
-  </script>
 </body>
 
 </html>
