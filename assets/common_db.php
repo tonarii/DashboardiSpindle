a71f1b2b4c86307156ce8b93804ead4819ee0718
tonarii <mydogsk8@hotmail.com>
2018-11-11 15:32:52 +0100

    Update v1.1.4

--- assets/common_db.php
+++ assets/common_db.php
@@ -1,16 +1,17 @@
 <?php
 
-// configure your database connection here:
-define('DB_SERVER',"SERVER");
-define('DB_NAME',"NAME");
-define('DB_USER',"USER");
-define('DB_PASSWORD',"PASSWORD");
+    // configure your database connection here:
+    define('DB_SERVER',"SERVER");
+    define('DB_NAME',"NAME");
+    define('DB_USER',"USER");
+    define('DB_PASSWORD',"PASSWORD");
 
-$conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
-if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-}
+    $conn = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
+    if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+    }
 
-define("defaultTimePeriod", 24); // Timeframe for chart (backwards from now)
-define("defaultReset",  false);  // Flag for Timeframe Start (beginning of chart display)
+    define("defaultTimePeriod", 24);    // Timeframe for chart (backwards from now)
+    define("defaultReset",  false);     // Flag for Timeframe Start (beginning of chart display)
+    define("defaultDaysAgo", 7);        // Default number of days past to look for active iSpindels
 ?>
